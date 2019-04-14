00de2d74941c1cdeac33b68ad7adad46cd3c7816
Deputatov Nikita <deputatovn@yandex.ru>
2019-04-14 20:17:54 +0300

    Added ex2.1

--- ch02/ex2.1/main.go
+++ ch02/ex2.1/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"github.com/deputatovn/gopl-solutions/ch02/ex2.1/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.CToK(20))
+	fmt.Println(tempconv.KToC(20))
+}
--- ch02/ex2.1/tempconv/tempconv.go
+++ ch02/ex2.1/tempconv/tempconv.go
@@ -10,8 +10,9 @@ const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
-func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g K", k) }
