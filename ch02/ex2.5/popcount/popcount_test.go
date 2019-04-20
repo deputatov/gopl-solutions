013f752b2a89d2bcc06cafab8e934f7e34e8ebdf
Deputatov Nikita <deputatovn@yandex.ru>
2019-04-20 20:27:27 +0300

    Added ex2.5

---
+++ ch02/ex2.5/popcount/popcount_test.go
@@ -0,0 +1,27 @@
+package popcount
+
+import "testing"
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByShifting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByShifting(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByClearing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByClearing(0x1234567890ABCDEF)
+	}
+}
