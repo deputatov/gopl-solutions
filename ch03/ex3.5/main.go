f05b840ccd4c160f8db1bdd484881d0c5eab9055
Deputatov Nikita <deputatovn@yandex.ru>
2019-04-26 20:34:21 +0300

    Added ex3.5

--- ch03/ex3.5/main.go
+++ ch03/ex3.5/main.go
@@ -1,1 +1,45 @@
-package ex3_5
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 2048, 2048
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		img.Set(5, 5, color.RGBA{54, 58, 48, 25})
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			cn := contrast * n
+			return color.RGBA{100 - cn, 100 - cn, 255 - cn, 255}
+
+		}
+	}
+	return color.Black
+}
